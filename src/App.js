9d72fb2ebe8c080459b99e1f61d74a69d53d8eb3
Mark Spicer <spicer93@gmail.com>
2023-06-22 00:38:30 -0400

    Fix build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 import Generative from './generative/Generative';
